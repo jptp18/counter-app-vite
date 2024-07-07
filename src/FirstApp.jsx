5352178fdadabee504a2c774bc68a58d89d9e9c1
Juan Trejo <juanpatp08@gmail.com>
2024-07-07 13:51:11 -0500

    Se agrega los botoness para sumar, restar y resetear el contador

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,18 +1,24 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ( {title} ) => {
+export const FirstApp = ( { title, subTitle} ) => {
 
 
 
   return (
   <>
     <h1>{title}</h1>
-    <p></p>
+    <p>{subTitle}</p>
   </>
 
   )
 }
 
 FirstApp.propTypes = {
-    title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+}
+
+FirstApp.defaultProps = {
+  title:'No hay titulo',
+  subTitle: 'No hay Subtitulo'
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
 import   './styles.css';
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot( document.getElementById("root") ).render(
     <React.StrictMode>
-        <FirstApp title="Fisrt App"/>
+        <CounterApp value={0}/>
     </React.StrictMode>
 )
