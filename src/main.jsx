07470321d1a9b7cbd27edbb8d2a62300143623e2
Juan Trejo <juanpatp08@gmail.com>
2024-07-22 22:00:59 -0500

    Se agregan configs para los test con react

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import   './styles.css';
 import { CounterApp } from './CounterApp';
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot( document.getElementById("root") ).render(
     <React.StrictMode>
-        <CounterApp value={0}/>
+        {/*<CounterApp value={0}/>*/}
+        <FirstApp title='Hola, soy Juan'/>
     </React.StrictMode>
 )
---
+++ tests/base-pruebas/11-async-await.test.js
@@ -0,0 +1,15 @@
+import { string } from "prop-types";
+import { getImagen } from "../../src/base-pruebas/11-async-await";
+
+describe('Pruebas sobre 11-async-await', () => { 
+   
+    test('getImagen debe retornar una url de la imagen', async () => { 
+        
+        const url = await getImagen();
+        console.log(url);
+
+        expect(typeof url).toBe('string');
+
+    })
+
+});
